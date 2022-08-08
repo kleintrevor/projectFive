9d445dd215bb3c7319d998b12c03f7014a276eca
kleintrevor <trevor.klein@gmail.com>
2022-08-08 04:03:31 -0500

    added raven movement

--- script.js
+++ script.js
@@ -1,4 +1,67 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = windows.innerWidth;
-canvas.height = windows.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let timeToNextRaven = 0;
+let ravenInterval = 500;
+let lastTime = 0;
+
+let ravens = [];
+class Raven {
+    constructor(){
+        this.width = 100;
+        this.height = 50;
+        this.x = canvas.width;
+        this.y = Math.random() * (canvas.height - this.height);
+        this.directionX = Math.random() * 5 + 3;
+        this.directionY = Math.random() * 5 - 2.5;
+
+    }
+    update(){
+        this.x -= this.directionX;
+
+    }
+    draw(){
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+const raven = new Raven();
+
+function animate(timestamp){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let deltatime = timestamp - lastTime;
+    lastTime = timestamp;
+    timeToNextRaven += deltatime;
+    // control for difference in power of device
+    if (timeToNextRaven > ravenInterval){
+        ravens.push(new Raven());
+        timeToNextRaven = 0;
+    }
+    [...ravens].forEach(object => object.update());
+    [...ravens].forEach(object => object.draw());
+    requestAnimationFrame(animate);
+}
+animate(0);
+
+
+/*
+would be good to use this for a frogger style game like you were thinking... *******dont forget this*******
+
+function animate(timestamp){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let deltatime = timestamp - lastTime;
+    lastTime = timestamp;
+    timeToNextRaven += deltatime;
+    // control for difference in power of device
+    if (timeToNextRaven > ravenInterval){
+        ravens.push(new Raven());
+        timeToNextRaven = 0;
+    }
+    [...ravens].forEach(object => object.update());
+    [...ravens].forEach(object => object.draw());
+    requestAnimationFrame(animate);
+}
+
+*/
