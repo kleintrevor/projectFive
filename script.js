9aa307cd47f3daf04703d0635a19eda21efd6e90
kleintrevor <trevor.klein@gmail.com>
2022-08-13 20:47:54 -0500

    updated for flap/ refresh speed, raven animation

--- script.js
+++ script.js
@@ -10,20 +10,37 @@ let lastTime = 0;
 let ravens = [];
 class Raven {
     constructor(){
+        this.spriteWidth = 271;
+        this.spriteHeight = 194;
+        this.sizeModifier = Math.random() * 0.6 + 0.4;
+        this.width = this.spriteWidth * this.sizeModifier;
+        this.height = this.spriteHeight * this.sizeModifier;
         this.width = 100;
         this.height = 50;
         this.x = canvas.width;
         this.y = Math.random() * (canvas.height - this.height);
         this.directionX = Math.random() * 5 + 3;
         this.directionY = Math.random() * 5 - 2.5;
-
+        this.markedForDeletion = false;
+        this.image = new Image();
+        this.image.src = 'raven.png';
+        this.frame = 0;
+        this.maxFrame = 4;
+        this.timeSinceFlap = 0;
+        this.flapInterval = 100;
     }
     update(){
         this.x -= this.directionX;
-
+        if (this.x < 0 - this.width) this.markedForDeletion = true;
+        this.timeSinceFlap += deltatime;
+        if (this.timeSinceFlap > this.flapInterval){
+            if (this.frame > this.maxFrame) this.frame = 0;
+            else this.frame++;            
+        }
     }
     draw(){
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, this.frame * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height)
     }
 }
 
@@ -39,8 +56,9 @@ function animate(timestamp){
         ravens.push(new Raven());
         timeToNextRaven = 0;
     }
-    [...ravens].forEach(object => object.update());
+    [...ravens].forEach(object => object.update(deltatime));
     [...ravens].forEach(object => object.draw());
+    ravens =  ravens.filter(object  => !object.markedForDeletion);
     requestAnimationFrame(animate);
 }
 animate(0);
